7a8f17cbb93eac013f88f33945177ba2b777c05e
Mateuslh <mateuslealhemkemeier@gmail.com>
2024-10-29 15:59:08 -0300

    fix - alterado tambem o controller de imagem

--- src/main/java/com/feiraFacil/controller/ImagemController.java
+++ src/main/java/com/feiraFacil/controller/ImagemController.java
@@ -4,7 +4,12 @@ import com.feiraFacil.dto.ResponseEntityDto;
 import com.feiraFacil.dto.baseEntity.ImagemResponseDTO;
 import com.feiraFacil.model.Imagem;
 import com.feiraFacil.service.ImagemService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +26,7 @@ public class ImagemController {
     @Autowired
     private ImagemService imagemService;
 
+    @Transactional
     @PostMapping("/upload")
     public ResponseEntityDto<ImagemResponseDTO> uploadImagem(@RequestParam("file") MultipartFile file) throws IOException {
         Imagem imagem = imagemService.salvarImagem(file);
@@ -28,6 +34,7 @@ public class ImagemController {
 
     }
 
+    @Transactional
     @GetMapping("/imagem/{id}")
     public ResponseEntity<byte[]> downloadImagem(@PathVariable Long id) {
         Imagem imagem = imagemService.obterImagem(id);
@@ -36,7 +43,7 @@ public class ImagemController {
                 .contentType(MediaType.parseMediaType(imagem.getTipo()))
                 .body(imagem.getDados());
     }
-
+    @Transactional
     @DeleteMapping("/imagem/{id}")
     public ResponseEntityDto<Void> delete(@PathVariable Long id) {
         imagemService.excluir(id);
